158d34ac88206b46000aeeed36f59ae4f3a20ec7
Florian <florian.seurre@git.epitech.eu>
2016-12-26 21:10:41 +0300

    begin interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  months = [
+    {code: '01', string: 'January'},
+    {code: '02', string: 'February'},
+    {code: '03', string: 'March'},
+    {code: '04', string: 'April'},
+    {code: '05', string: 'May'},
+    {code: '06', string: 'June'},
+    {code: '07', string: 'July'},
+    {code: '08', string: 'August'},
+    {code: '09', string: 'September'},
+    {code: '10', string: 'October'},
+    {code: '11', string: 'November'},
+    {code: '12', string: 'December'},
+  ];
+  selectedMonth = "January";
 }
